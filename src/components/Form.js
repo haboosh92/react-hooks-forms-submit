1ffb0f745143e373dfab051fb17583c87036853b
Hiba <eng.lilil92@gmail.com>
2022-10-30 02:34:39 +0300

    completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,22 +3,57 @@ import React, { useState } from "react";
 function Form(props) {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submData, setSubmData] = useState([]);
+  const [err,setErr] = useState([])
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
   }
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
   }
 
+  function handleSubmit(event){
+    event.preventDefault()
+    if (firstName.length > 0) {
+      const formData ={
+        firstName:firstName,
+        lastName:lastName,
+      }
+      const dataArray = [...submData, formData]
+      setSubmData(dataArray);
+      setFirstName("");
+      setLastName("");
+      setErr([""]);
+    } else {
+      setErr(["First name is required!"]);
+    }
+  }
+
+  const listOfSubmissions = submData.map((data,index)=>{
+    return (
+      <div key={index}>
+      {data.firstName} {data.lastName}
+      </div>
+    )
+  })
   return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
-    </form>
-  );
-}
-
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+        <h3>Submissions </h3>
+        {listOfSubmissions}
+      </form>
+      {err.length > 0
+        ? err.map((err, index) => (
+            <p key={index} style={{ color: "red" }}>
+              {err}
+            </p>
+          ))
+        : null}
+    </div>
+  ); }
 export default Form;
